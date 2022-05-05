768b54709e89e341480ba0b5021baf3d0e2fb3b8
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-05-05 11:33:30 +0200

    WireMock Test

--- WireMock-Rest/src/test/java/com/kingcode/wiremockrest/AzureTranslateTest.java
+++ WireMock-Rest/src/test/java/com/kingcode/wiremockrest/AzureTranslateTest.java
@@ -3,6 +3,9 @@ package com.kingcode.wiremockrest;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -22,6 +25,10 @@ public class AzureTranslateTest {
     public void testTranslate() {
         //Given
         wireMock.stubFor(post(urlEqualTo(AZURE_PATH))
+            .withHeader("Ocp-Apim-Subscription-Key", equalTo("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"))
+            .withHeader("Ocp-Apim-Subscription-Region", equalTo("westeurope"))
+            .withHeader("Content-Type", equalTo("application/json"))
+            .withRequestBody(equalToJson("[{'Text':'Funfzehn jahre alt'}]"))
             .willReturn(aResponse()
                 .withHeader("content-type", "application/json")
                 .withBody(
@@ -31,14 +38,19 @@ public class AzureTranslateTest {
 
         //When
         RestTemplate rest = new RestTemplate();
-        String response = rest.postForObject(
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Ocp-Apim-Subscription-Key", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        headers.add("Ocp-Apim-Subscription-Region", "westeurope");
+        headers.add("Content-Type", "application/json");
+        ResponseEntity<String> response = rest.postForEntity(
             "http://localhost:" + wireMock.getRuntimeInfo().getHttpPort() + "/api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en",
-            null,
+            new HttpEntity<>("[{'Text': 'Funfzehn jahre alt'}]", headers),
             String.class
         );
 
+
         //Then
-        assertThat(response).isEqualTo(
+        assertThat(response.getBody()).isEqualTo(
             "[{\"detectedLanguage\":{\"language\":\"de\",\"score\":1.0},\"translations\":[{\"teeen " +
                 "years old\",\"to\":\"en\"}]}]");
     }
